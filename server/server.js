521d6e4e1d48162f98a30a9d1ef101f4e835f457
Kiran <kiran.devkar@flowian.com>
2017-03-20 16:45:40 +0530

    'Separated_workflows_and_use_restAPI'

---
+++ server/server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {todo} = require('./models/todo');
+var {user} = require('./models/user');
+
+var app = express();
+
+app.use(bodyparser.json());
+
+app.post('/todos',(req,res) =>{
+
+    var ntodo = new todo({
+        text : req.body.text
+    });
+
+    ntodo.save().then((doc)=>{
+       res.send(JSON.stringify(doc,undefined,2));
+    },(e) =>{
+       res.send('Error While Saving DAta',e);
+    });
+})
+
+
+
+
+app.listen(3000 , ()=>{
+    console.log('Listeing port 3000');
+});
+// var newuser = new user({
+   
+// });
+
+// newuser.save().then((doc)=>{
+//     console.log('result of save is ',doc);
+// },(e) =>{
+//     console.log(e);
+// });
+
+// var newtodo = new todo({
+//     text :'Cook dinner'
+// });
+
+// newtodo.save().then((doc)=>{
+//     console.log('result of save is ',doc);
+// },(e) =>{
+//     console.log(e);
+// });
+
+// var newtodo = new todo({
+//     text :'Kiran text'
+// });
+
+// newtodo.save().then((doc)=>{
+//     console.log('result of save is ',doc);
+// },(e) =>{
+//     console.log(e);
+// });
